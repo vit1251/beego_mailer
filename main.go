9a86e2efe45a1e93d852696b5e33c5bda769cc50
Vitold S <vit1251@gmail.com>
2018-12-12 08:31:40 +0300

    Update

--- main.go
+++ main.go
@@ -1,2 +1,21 @@
 package beego_mailer
 
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+)
+
+func InitMailerQueue(ctx *context.Context) {
+}
+
+func ProcessMailerQueue(ctx *context.Context) {
+}
+
+func InitMailerFilter() {
+
+	beego.InsertFilter("*", beego.BeforeRouter, InitMailerQueue)
+	beego.InsertFilter("*", beego.FinishRouter, ProcessMailerQueue, false)
+
+	beego.Info("Keenio filter initialized")
+
+}
